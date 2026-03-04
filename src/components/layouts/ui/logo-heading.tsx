350c88256d528c4bc28b749f6182f22bd1161abc
ParkerGits <parkerjlandon@gmail.com>
2026-03-04 09:59:08 -0800

    optimize lighthouse

--- src/components/layouts/ui/logo-heading.tsx
+++ src/components/layouts/ui/logo-heading.tsx
@@ -1,10 +1,10 @@
 export default function LogoHeading() {
   return (
-    <a href="/">
-      <nav className="flex flex-row items-center justify-center group">
+    <nav aria-label="Home">
+      <a href="/" className="flex flex-row items-center justify-center group">
         <img src="/logo.svg" alt="Lordly Logo" className="w-auto h-[8vh] aspect-square" />
         <h2 className="font-logo text-xl hidden sm:block group-hover:text-highlight">Parker&apos;s Digital Domain</h2>
-      </nav>
-    </a>
+      </a>
+    </nav>
   );
 }
